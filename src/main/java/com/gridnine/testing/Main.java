24508a079d5eed11eae942545d8199a8e7568bed
asbyvshev <barseto4ka@live.ru>
2020-09-26 13:42:25 +0300

    FlightFilter.Task1.Определил задачи по проекту.

--- src/main/java/com/gridnine/testing/Main.java
+++ src/main/java/com/gridnine/testing/Main.java
@@ -1,4 +1,12 @@
 package com.gridnine.testing;
 
 public class Main {
+    public static void main(String[] args) {
+
+        // 1.Создаем полет.
+        // 2.Создаем FilterManager (управляет фильтрами: добавляет\убирает).
+        // 3.Создаем нужные фильтры.
+        // 4.Добавляем в FilterManager фильтры.
+        // 5.Применяем FilterManager к полету.
+    }
 }
